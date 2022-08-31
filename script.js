76a9f0e010990de1ca59ddddc946a7402b2a1345
Alisson Evangelista <108416106+AlissonED@users.noreply.github.com>
2022-08-31 10:08:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function calcSum() {
+    let num1 = document.getElementsByName("n1")[0].value;
+    let num2 = document.getElementsByName("n2")[0].value;
+
+    let soma = Number(num1) + Number(num2)
+
+    document.getElementsByName("re")[0].value = soma;
+}
